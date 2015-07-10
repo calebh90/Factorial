a3cfdc0f4216ecc9271f77aaa509c2d25fcbcf2d
unknown <chall@09HALLC.cohnreznick.net>
2015-07-10 11:49:23 -0500

    Initial commit

---
+++ fact.py
@@ -0,0 +1,16 @@
+from math import factorial
+from cmath import cos
+
+fact = factorial
+fact2 = cos
+n = int(input("What number do you want to factor? "))
+print("The factor of", n , "is" , fact(n))
+
+Quest = input("Do you want to do know the cosine of " + str(n) + "? ").lower()
+def cosfunc(x):
+	if x == "yes":
+		return ("Ok, the cosine of "+ str(n) + " is " + str(fact2(n)))
+	else:
+		return "Ok, have a good day! :)"
+
+print(cosfunc(Quest))
