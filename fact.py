ae5f225cee05afb2f04a1cadf11716781cc80ef4
Caleb <chall@09HALLC.cohnreznick.net>
2015-07-10 12:04:08 -0500

    Changed cosine message

--- fact.py
+++ fact.py
@@ -6,7 +6,7 @@ fact2 = cos
 n = int(input("Hi! What number do you want to factor? "))
 print("The factor of", n , "is" , fact(n))
 
-Quest = input("Do you want to do know the cosine of " + str(n) + "? ").lower()
+Quest = input("Do you also want to do know the cosine of " + str(n) + "? ").lower()
 def cosfunc(x):
 	if x == "yes":
 		return ("Ok, the cosine of "+ str(n) + " is " + str(fact2(n)))
